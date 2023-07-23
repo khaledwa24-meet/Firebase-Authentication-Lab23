b2d6fe52d5c52661f39ffca954671038e1974a87
khaledwa24-meet <139680869+khaledwa24-meet@users.noreply.github.com>
2023-07-23 13:25:32 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+dictionary =  {
+"apiKey": "AIzaSyBP8jkjzHxGltZn7_IMUbFAtGeeWjbpvR4",
+  "authDomain": "banana-af64f.firebaseapp.com",
+  "projectId": "banana-af64f",
+  'storageBucket': "banana-af64f.appspot.com",
+  'messagingSenderId': "866519119331",
+  'appId': "1:866519119331:web:a0ed17b1abffe163dadcd6",
+  'measurementId': "G-2XGQD7LMFK",
+"databaseURL" : ""
+}
+
+firebase = pyrebase.initialize_app(dictionary)
+auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
