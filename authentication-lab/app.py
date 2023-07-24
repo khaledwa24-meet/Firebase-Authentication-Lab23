69a1311941ed529dc5923f7bd4155ce98e480a97
khaledwa24-meet <139680869+khaledwa24-meet@users.noreply.github.com>
2023-07-25 01:09:58 +0300

    im done with the bonus too im just gonna ask a small question about the bones tomorrow
    :)

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -21,10 +21,19 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
-
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return render_template("add_tweet.html")
+        except:
+            error = "oppsie smol mistake try agean"
+            return render_template("signin.html")
+    else:
+        return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -34,6 +43,13 @@ def signup():
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+@app.route('/sign_out', methods=['GET', 'POST'])
+def sign_out():
+    auth.user = None
+    login_session['user'] = None
+    return render_template('signin.html')
+
 
 
 if __name__ == '__main__':
